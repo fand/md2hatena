90960a3e44ad77358b01ba7508efed78bb8d15d3
amagitakayosi <fand.gmork@gmail.com>
2017-02-28 21:31:41 +0900

    Add test for md2hatena result

--- test/heading.js
+++ test/heading.js
@@ -1,5 +1,6 @@
 import test from 'ava';
-import { nodeToHatena } from '../src';
+import md2hatena, { nodeToHatena } from '../src';
+import 'babel-polyfill';
 
 test('nodeToHatena compiles node', t => {
   const node = { type: 'heading', children: [{ type: 'text', value: 'yo' }] };
@@ -9,4 +10,13 @@ test('nodeToHatena compiles node', t => {
   t.is(nodeToHatena({ ...node, depth: 4 }), '\n**** yo\n');
   t.is(nodeToHatena({ ...node, depth: 5 }), '\n***** yo\n');
   t.is(nodeToHatena({ ...node, depth: 6 }), '\n****** yo\n');
+});
+
+test('md2hatena compiles string', async t => {
+  t.is(await md2hatena('# yo'), '\n* yo\n');
+  t.is(await md2hatena('## yo'), '\n** yo\n');
+  t.is(await md2hatena('### yo'), '\n*** yo\n');
+  t.is(await md2hatena('#### yo'), '\n**** yo\n');
+  t.is(await md2hatena('##### yo'), '\n***** yo\n');
+  t.is(await md2hatena('###### yo'), '\n****** yo\n');
 });
