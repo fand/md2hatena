c608431b6442d3fd4e425e3ece81fcfef17bdb99
amagitakayosi <fand.gmork@gmail.com>
2017-02-28 21:33:51 +0900

    Add tests for text node

---
+++ test/text.js
@@ -0,0 +1,11 @@
+import test from 'ava';
+import md2hatena, { nodeToHatena } from '../src';
+import 'babel-polyfill';
+
+test('nodeToHatena(text)', t => {
+  t.is(nodeToHatena({ type: 'text', value: 'yo' }), 'yo');
+});
+
+test('md2hatena(text)', async t => {
+  t.is(await md2hatena('yo'), 'yo\n');
+});
