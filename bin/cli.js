8fc465add566424ed593030fd9d464134de05cc4
amagitakayosi <fand.gmork@gmail.com>
2017-02-28 19:09:56 +0900

    init

---
+++ bin/cli.js
@@ -0,0 +1,14 @@
+const remark = require('remark');
+const fs = require('fs');
+const p = require('pify');
+const md2hatena = require('../src/md2hatena');
+
+p(fs.readFile)(process.argv[2], 'utf8')
+  .then(p(remark().use(md2hatena).process))
+  .then((data) => {
+    console.log(data.contents);
+  })
+  .catch((err) => {
+    console.log('ERROR: failed to compile markdown file');
+    console.error(err);
+  });
---
+++ src/index.js
@@ -0,0 +1,1 @@
+module.exports = require('./md2hatena');
